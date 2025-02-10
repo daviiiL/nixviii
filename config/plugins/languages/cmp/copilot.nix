43ab9bbca1fd0b3704e66f78fb2efd2296413c7c
chronos <davidas99017@gmail.com>
2025-02-10 15:09:56 -0500

    feat(copilot): adding copilot responses to cmp

--- config/plugins/languages/cmp/copilot.nix
+++ config/plugins/languages/cmp/copilot.nix
@@ -10,10 +10,10 @@
     copilot-cmp.enable = true;
   };
 
-  extraConfigLua = ''
-    require("copilot").setup({
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-    })
-  '';
+  # extraConfigLua = ''
+  #   require("copilot").setup({
+  #     suggestion = { enabled = false },
+  #     panel = { enabled = false },
+  #   })
+  # '';
 }
